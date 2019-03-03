9400c8fdc52927f995dfa2178a8f2ffbfcaa1cfa
xianjiezh <904292766@qq.com>
2019-03-03 21:13:18 +0800

    添加了icon

--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,30 @@
 <template>
-  <button class="n-button">
-    <slot></slot>
+  <button class="n-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
 <script>
 export default {
-  name: "n-button"
+  name: "n-button",
+  props: {
+    icon: {},
+    iconPosition: {
+      default: 'left',
+      validator(value) {
+        return value == 'left' || value == 'right'
+      }
+    }
+  }
 };
 </script>
-
-<style lang="scss">
+  
+<style lang="scss" scoped>
 .n-button {
   height: var(--button-height);
   font-size: var(--font-size);
@@ -18,6 +32,34 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &.icon-left {
+    >.icon {
+      order: 1;
+    }
+    >.content {
+      order: 2;
+    }
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+    }
+    >.content {
+      order: 1;
+    }
+  }
+  > .icon {
+    vertical-align: middle;
+    order: 1;
+  }
+  > .content {
+    padding-left: .2em;
+    padding-right: .2em;
+  }
   &:hover {
     border-color: var(--border-color-hover);
   }
---
+++ src/icon.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'icon',
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
